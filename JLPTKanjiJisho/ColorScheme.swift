e7df20a59babffdc4499ed1d1ba2fe7bac03a573
cpg6b <chris.garvey@gmail.com>
2016-03-23 23:17:54 -0700

    feat: Add basic search view controller and color scheme

---
+++ JLPTKanjiJisho/ColorScheme.swift
@@ -0,0 +1,17 @@
+//
+//  ColorScheme.swift
+//  JLPTKanjiJisho
+//
+//  Created by Chris Garvey on 3/23/16.
+//  Copyright © 2016 Chris Garvey. All rights reserved.
+//
+
+import UIKit
+
+struct ColorScheme {
+    static let crimson: UIColor = UIColor(red: 0.557, green: 0.0, blue: 0.067, alpha: 1.0)
+    static let cherryRed: UIColor = UIColor(red: 0.796, green: 0.086, blue: 0.075, alpha: 1.0)
+    static let cream: UIColor = UIColor(red: 0.925, green: 0.914, blue: 0.898, alpha: 1.0)
+    static let ash: UIColor = UIColor(red: 0.702, green: 0.635, blue: 0.647, alpha: 1.0)
+}
+
---
+++ JLPTKanjiJisho/SearchViewController.swift
@@ -0,0 +1,73 @@
+//
+//  SearchViewController.swift
+//  JLPTKanjiJisho
+//
+//  Created by Chris Garvey on 3/23/16.
+//  Copyright © 2016 Chris Garvey. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController, UITextFieldDelegate {
+
+    // MARK: - Properties
+    
+    @IBOutlet weak var appTitle: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /* Configure the UI */
+        configureUI()
+        
+        /* Set Text Field Delegate */
+        searchTextField.delegate = self
+        
+        /* Enable Tap Gesture Recognizer */
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(SearchViewController.tap(_:)))
+        view.addGestureRecognizer(tapGesture)
+        
+    }
+
+    
+    // MARK: - Actions
+    
+    @IBAction func search(sender: UIButton) {
+        
+        // TO DO: add search functionality
+        
+    }
+    
+    
+    // MARK: - Helper Functions
+    
+    /* Dismiss keyboard if user touches outside the text field */
+    func tap(gesture: UITapGestureRecognizer) {
+        if searchTextField.isFirstResponder() == true {
+            searchTextField.resignFirstResponder()
+        }
+    }
+    
+    func configureUI() {
+        /* Configure background gradient */
+        view.backgroundColor = UIColor.clearColor()
+        let colorTop = ColorScheme.cherryRed.CGColor
+        let colorBottom = ColorScheme.crimson.CGColor
+        let backgroundGradient = CAGradientLayer()
+        backgroundGradient.colors = [colorTop, colorBottom]
+        backgroundGradient.locations = [0.0, 1.0]
+        backgroundGradient.frame = view.frame
+        view.layer.insertSublayer(backgroundGradient, atIndex: 0)
+
+        /* Set Element Colors */
+        appTitle.textColor = ColorScheme.cream
+        searchTextField.textColor = ColorScheme.cream
+        searchTextField.backgroundColor = ColorScheme.ash
+    }
+}
+
