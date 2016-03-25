96e54d23e612c9f14077b366926eff2b0dce932b
cpg6b <chris.garvey@gmail.com>
2016-03-24 23:11:24 -0700

    feat: Create parameters of kanji data model

---
+++ JLPTKanjiJisho/MasterKanjiList.swift
@@ -0,0 +1,30 @@
+//
+//  MasterKanjiList.swift
+//  JLPTKanjiJisho
+//
+//  Created by Chris Garvey on 3/24/16.
+//  Copyright © 2016 Chris Garvey. All rights reserved.
+//
+
+import Foundation
+
+/* Global Constant */
+let masterKanjiList: [[String:[[String]]]] =
+    [
+        
+        // MARK: - JLPT N5
+        
+        ["水": [["N5"],["スイ"],["みず"],["water"]]],
+        ["年": [["N5"],["オン"],["とし"],["year"]]],
+        ["半": [["N5"],["ハン"],["なか.ば"],["half","middle"]]],
+        ["日": [["N5"],["ニチ","ジツ"],["ひ","-び","-か"],["day","sun","Japan","counter for days"]]]
+        
+        // MARK: - JLPT N4
+        
+        // MARK: - JLPT N3
+
+        // MARK: - JLPT N2
+        
+        // MARK: - JLPT N1
+        
+]
